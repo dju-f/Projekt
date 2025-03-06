ef50e167c5ea5f3330fe22a263a588ed3f8f7dff
Speer07 <q.rellstab@gmail.com>
2025-03-06 09:51:19 +0100

    Mit Test

--- form_receive.php
+++ form_receive.php
@@ -9,6 +9,42 @@
             $cnumber = $_POST["cnumber"];
             $cvv = $_POST["cvv"];
             $expirydate = $_POST["expirdate"];
+
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "Conspiracy_web";
+            
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+              die("Connection failed: " . $conn->connect_error);
+            }
+            
+            $sql = "INSERT INTO User (User_Username, User_Email)
+            VALUES ('$user', '$email')";
+            
+            if ($conn->query($sql) === TRUE) {
+              echo "New record created successfully";
+            } else {
+              echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+            
+            $get = $conn->query("SELECT User_Username FROM User;");
+
+            // Check if the query returned any rows
+            if ($get->num_rows > 0) {
+                // Output data for each row
+                while($row = $get->fetch_assoc()) {
+                    echo "Username: " . $row["User_Username"] . "<br>";
+                }
+            } else {
+                echo "No users found.";
+            }
+
+            $conn->close(); 
         ?>
+    
     </body>
 </html>
