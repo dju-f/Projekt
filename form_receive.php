bb003a42e51c40bca38cc4a6b863c3e0e6c7893b
Speer07 <q.rellstab@gmail.com>
2025-03-06 08:17:51 +0100

    first form version

---
+++ form_receive.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+    Sie sind angemeldet!
+        <?php 
+            $user = $_POST["user"];
+            $email = $_POST["email"];
+            $fname = $_POST["fname"];
+            $lname = $_POST["lname"];
+            $cnumber = $_POST["cnumber"];
+            $cvv = $_POST["cvv"];
+            $expirdate = $_POST["expirdate"];
+        ?>
+    </body>
+</html>
