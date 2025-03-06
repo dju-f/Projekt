4abd3a26845545b666b29c9fc4f691686c0e7a4f
Speer07 <q.rellstab@gmail.com>
2025-03-06 08:34:10 +0100

    formating 2

--- form_input.php
+++ form_input.php
@@ -3,15 +3,15 @@
 
         <form action="form_receive.php" method="post">
             Personalien:<br>
-                Benutzername:   <input type="text" name="user"><br>
-                E-mail:         <input type="text" name="email"><br>
+                Benutzername:                       <input type="text" name="user"><br>
+                E-mail:                             <input type="text" name="email"><br>
             Kontodaten:<br>
-                Vorname:        <input type="text" name="fname"><br>
-                Nachname:       <input type="text" name="lname"><br>
-                Kartennummer (Ohne Lehrzeichen):   <input type="text" name="cnumber"><br>
-                CVV:            <input type="text" name="cvv"><br>
-                Ablaufdatum (MM/JJJJ):    <input type="text" name="expirdate"><br>
-                                <input type="submit">
+                Vorname:                            <input type="text" name="fname"><br>
+                Nachname:                           <input type="text" name="lname"><br>
+                Kartennummer (Ohne Lehrzeichen):    <input type="text" name="cnumber"><br>
+                CVV:                                <input type="text" name="cvv"><br>
+                Ablaufdatum (MM/JJ):                <input type="text" name="expirdate"><br>
+                                                    <input type="submit">
         </form>
 
     </body>
