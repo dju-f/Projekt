710a8926ede022f19663707cf9fe8bad948b9750
Speer07 <q.rellstab@gmail.com>
2025-03-08 19:13:20 +0100

    without check

--- comments_receive.php
+++ comments_receive.php
@@ -1,55 +1,31 @@
-<html>
-    <body>
-        <?php 
-            try{
-                $author = $_POST["author"];
-                $title = $_POST["title"];
-                $text = $_POST["text"];
-                
-                if ($author == "" and $title != "" and $text != "") {
-                    $author = "Anonym";
-                }
+<?php 
+        $author = $_POST["author"];
+        $title = $_POST["title"];
+        $text = $_POST["text"];
+        
+        if ($author == "" and $title != "" and $text != "") {
+            $author = "Anonym";
+        }
 
-                elseif ($author != ""){
+        elseif($title != "" or $text != "") {}
 
-                    include("connection.php");
-                    
-                        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+        else{
+            include("connection.php");
+            
+                mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-                        $sql = "INSERT INTO forum (forum_author, forum_title, forum_text)
-                        VALUES ('$author', '$title', '$text')";
+                $sql = "INSERT INTO forum (forum_author, forum_title, forum_text)
+                VALUES ('$author', '$title', '$text')";
 
-                        try {
-                            mysqli_query($conn, $sql);
-                        } 
-                        catch (mysqli_sql_exception $e) {
-                            echo "Verbindung zum Server fehlgeschlagen!" . $e->getMessage();
-                        }
-
-                    mysqli_close($conn);
+                try {
+                    mysqli_query($conn, $sql);
                 } 
+                catch () {}
 
-                else{};
-            }
-            finally{
-                include("connection.php");
+            mysqli_close($conn);
+        } 
 
-                    $sql = "SELECT * FROM forum";            
-                    $result = mysqli_query($conn, $sql);
-                    if(mysqli_num_rows($result) > 0) {
-                        while($row = mysqli_fetch_assoc($result)){
-                        echo $row["forum_author"];
-                        echo $row["forum_title"];
-                        echo $row["forum_text"];
-                        };
-                    }
-                    else{
-                        echo "Ladefehler!";
-                    }
 
-                mysqli_close($conn);
-            };
-        ?>
-    
-    </body>
-</html>
+    header("Location: examplepage.php");
+    die();
+?>
