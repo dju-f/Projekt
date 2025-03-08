edd6b9356d82d480d4f30a56cc5fda824c872f89
Speer07 <q.rellstab@gmail.com>
2025-03-08 14:57:57 +0100

    fix

--- connection.php
+++ connection.php
@@ -3,6 +3,7 @@
     $dbuser = "root";
     $dbpass = "";
     $dbname = "conspiracy_web";
+    $conn = "";
 
     try{$conn = mysqli_connect($dbserver, $dbuser, $dbpass, $dbname)}
     catch(mysqli_sql_exeption){echo "Verbindung zum Server fehlgeschlagen!"}
