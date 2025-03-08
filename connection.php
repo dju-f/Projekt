3102c7b4a12d84374c1a012dd0f1403d5d2e6189
Speer07 <q.rellstab@gmail.com>
2025-03-08 14:52:29 +0100

    File for server connection

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+    $dbserver = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $dbname = "conspiracy_web";
+
+    try{$conn = mysqli_connect($dbserver, $dbuser, $dbpass, $dbname)}
+    catch(mysqli_sql_exeption){echo "Verbindung zum Server fehlgeschlagen!"}
+?>
