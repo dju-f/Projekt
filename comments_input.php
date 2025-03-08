30f0881f4fae166f9b18498efc18f0df388ae3f1
Speer07 <q.rellstab@gmail.com>
2025-03-08 16:16:01 +0100

    typo

--- comments_input.php
+++ comments_input.php
@@ -3,7 +3,7 @@
 
         <form action="comments_receive.php" method="post">
             Autor (Kann leer bleiben.):             <input type="text" name="author"><br>
-            Titel:                                  <input type="text" name="titel"><br>
+            Titel:                                  <input type="text" name="title"><br>
             Text:                                   <input type="text" name="text"><br>
                                                     <input type="submit">
         </form>
