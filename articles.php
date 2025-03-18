206a5bbfc267856d5dd6a2f314575422896e06a2
Speer07 <q.rellstab@gmail.com>
2025-03-18 23:00:02 +0100

    Artikel -> Web-Artikel

--- articles.php
+++ articles.php
@@ -27,9 +27,9 @@
         $sql = "SELECT * FROM articles";
         $result = mysqli_query($conn, $sql);
         if(mysqli_num_rows($result) > 0) {
-            // Hier werden alle Artikel mit all ihren Atributen angezeigt und danach hört die Schlaufe auf.
+            // Hier werden alle Web-Artikel mit all ihren Atributen angezeigt und danach hört die Schlaufe auf.
             while($row = mysqli_fetch_assoc($result)){
-                // Hier liegt der Link zu einem einzelnen Artikel in article.php.
+                // Hier liegt der Link zu einem einzelnen Web-Artikel in article.php.
                 echo "<a href=article.php?id=".$row["article_id"]." class=\"clickable_div\" style=\"all:inherit\">";
                 echo "<div class=\"product_list_item\">";
                 echo "<img src=\"" . $row["article_image"] . "\"/>";
@@ -47,6 +47,6 @@
     mysqli_close($conn);
 ?>
 <html>
-        <!-- Hier befindet sich die Fusszeile. -->
+    <!-- Hier befindet sich die Fusszeile. -->
     <p class="footer"><br><br><br><br><br>© 2025 ConspiracyWeb<br><br></p>
 </html>
